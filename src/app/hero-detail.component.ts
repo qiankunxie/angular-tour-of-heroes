c1e5fed0b13404f578f5da6ab053508938783ae9
Qiankun Xie <xieqiankun@wustl.edu>
2017-02-13 23:31:02 -0600

    routing

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,22 +1,25 @@
-import { Component, Input } from '@angular/core';
-import { Hero } from './hero';
+// Keep the Input import for now, we'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
 
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
 @Component({
   selector: 'my-hero-detail',
   template: `
-  <div *ngIf="hero">
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name"/>
+    <div *ngIf="hero">
+      <h2>{{hero.name}} details!</h2>
+      <div>
+        <label>id: </label>{{hero.id}}
+      </div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name"/>
+      </div>
     </div>
-  </div>
-`
-
+  `
 })
 export class HeroDetailComponent {
-	@Input()
-  	hero: Hero;
-
+  @Input() hero: Hero;
 }
